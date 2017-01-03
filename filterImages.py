661e2c9ecb236e2c644841b9f6d3d0bfd3f71797
chickens2 <thechickenofdoom222@gmail.com>
2017-01-03 00:11:20 -0500

    stuff

--- filterImages.py
+++ filterImages.py
@@ -3,16 +3,16 @@ import os
 from shutil import copyfile
 import sys
 from pprint import pprint
-
 filelist = [ f for f in os.listdir("sortedImages")]
 for f in filelist:
-    os.remove(f)
-allfiles=[]
+    os.remove('sortedImages/'+f)
+allfilelists=[]
 for arg in sys.argv[1:]:
 	print arg
 	tags=iterate621.imageByTags.keys()
 	if arg in tags:
-		allfiles.extend(iterate621.imageByTags[arg])
+		allfilelists.append(set(iterate621.imageByTags[arg]))
+allfiles=set.intersection(*allfilelists)
 pprint(allfiles)
 for file in allfiles:
 	copyfile('cachedData/'+file,'sortedImages/'+file.replace('jpg','.jpg'))
